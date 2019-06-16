2da3aaf7a450b438d53476d3b9f0446c6c0ed750
Lucas Fernando <lucaslopes116@hotmail.com>
2019-06-16 20:04:04 -0300

    fix do fix do fix url

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -55,7 +55,7 @@ class Feed extends Component {
                        <img src={more} alt="Mais"/>
                    </header>
 
-                   <img src={`http://localhost:3333/files/${post.image}`} alt=""/>
+                   <img src={`${api}${post.image}`} alt=""/>
 
                    <footer>
                        <div className="actions">
